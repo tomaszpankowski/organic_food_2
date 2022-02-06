15d0e7ef588ecd2ac63aa928f4bb2b933ee9d348
tomaszpankowski <pankowski.email@gmail.com>
2022-02-06 11:10:30 +0100

    layout done

--- src/views/food.js
+++ src/views/food.js
@@ -1,11 +1,16 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import FoodS1 from "../components/food-s1";
+import FoodS2 from "../components/food-s2";
+import FoodS3 from "../components/food-s3";
 
 class Food extends Component{
     render(){
         return(        
-            <Container fluid className="food-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <FoodS1/>
+                <FoodS2/>
+                <FoodS3/>
             </Container>    
         );
     }
